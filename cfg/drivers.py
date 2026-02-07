2e948bd61d1b252e3ad9661eb9a894704bbd2915
Brandon T <brandonrtand@gmail.com>
2026-02-06 19:16:45 -0500

    Update drivers.py

--- cfg/drivers.py
+++ cfg/drivers.py
@@ -1,15 +1,15 @@
 import subprocess
 
 def cpu_microcode_packages():
-    print("Detecting CPU model for appropriate microcode package installation.")
+    #print("Detecting CPU model for appropriate microcode package installation.")
     try:
-        with open("proc/cpuinfo","r") as f:
+        with open("/proc/cpuinfo","r") as f:
             cpu_info = f.read()
         if "AMD" in cpu_info:
-            print("AMD CPU detected, installing amd-ucode.")
+            #print("AMD CPU detected, installing amd-ucode.")
             return["amd-ucode"]
         elif "Intel" in cpu_info:
-            print("Intel CPU detected, installing intel-ucode.")
+            #print("Intel CPU detected, installing intel-ucode.")
             return["intel-ucode"]
         else:
             return[]
@@ -17,20 +17,21 @@ def cpu_microcode_packages():
         return[]
     
 def gpu_driver_packages():
-    print("Detecting GPU model for appropriate driver package installation.")
+    #print("Detecting GPU model for appropriate driver package installation.")
     drivers = []
     try:
         lspci = subprocess.check_output(["lspci"], text=True).lower()
     except:
-        print("Failed to run lspci, skipping GPU detection.")
+        #print("Failed to run lspci, skipping GPU detection.")
         return[]
 
     if "nvidia" in lspci:
-        print("NVIDIA GPU detected.")
+        #print("NVIDIA GPU detected.")
         drivers.extend(["nvidia","nvidia-utils","nvidia-settings"])
     if "amd" in lspci or "radeon" in lspci:
-        print("AMD GPU detected.")
+        #print("AMD GPU detected.")
         drivers.extend(["mesa","vulkan-radeon","libva-mesa-driver"])
     # Intel support is NOT added yet.
-    print(f"Requesting {drivers} to be installed.")
+    #print(f"Requesting {drivers} to be installed.")
     return drivers
+    
