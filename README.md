05dd20365669263f299d6d3cdd47af2f316e1943
itsbt125 <itsbt125@users.noreply.github.com>
2026-02-27 22:20:58 -0500

    QOL Improvements and bug fixes

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 **Arch Linux CLI Installer**
 
-An intuitive, Python-based command-line installer for Arch Linux, designed with beginners in mind.
-This tool allows you to configure and install Arch with ease while setting up a functional [KDE Plasma](https://kde.org/plasma-desktop/) system in the background. It removes the complexity Arch is known for while keeping the lightweight nature of the OS.
+An intuitive, Python 3-based command-line installer for Arch Linux, built with beginners in mind.
+This tool allows you to configure and install Arch with ease while setting up a functional [KDE Plasma](https://kde.org/plasma-desktop/) environment automatically. It removes the complexity Arch is known for while keeping the lightweight nature of the OS.
 
-
-How to use this:
+Usage steps:
 1. Boot into the Arch Linux live environment using your installation medium.
-2. If ethernet is unavailable, connect to the internet using `iwctl` for WiFi. I suggest checking the [ArchWiki](https://wiki.archlinux.org/title/Iwd) page for this.
+2. If Ethernet is unavailable, connect to the internet using `iwctl` for WiFi. It's recommended to check the [ArchWiki](https://wiki.archlinux.org/title/Iwd) page for this.
 3. Run the following one-line command ```pacman -Sy git --noconfirm && git clone https://github.com/itsbt125/Arch-Linux-CLI-Installer && cd Arch-Linux-CLI-Installer && python main.py```
+
--- cfg/config.py
+++ cfg/config.py
@@ -4,29 +4,23 @@ from cfg.cmds import cmd
 def run_chroot(command): 
     full_cmd = ["arch-chroot", "/mnt"] + command
     try:
-        #subprocess.run(full_cmd, check=True)
         cmd(full_cmd)
     except subprocess.CalledProcessError as e:
         print(f"[!] Chroot command failed: {' '.join(cmd)}")
         raise e
-
 def generate_fstab():
     print("[-] Generating fstab...")
-    #subprocess.run("genfstab -U /mnt > /mnt/etc/fstab", shell=True, check=True)
     cmd("genfstab -U /mnt > /mnt/etc/fstab",shell=True)
 def configure_system(hostname, username, user_password, root_password, timezone="America/New_York", locale="en_US.UTF-8", keymap="us"):
     print(f"[-] Configuring system: {hostname}")
     run_chroot(["ln", "-sf", f"/usr/share/zoneinfo/{timezone}", "/etc/localtime"])
     run_chroot(["hwclock", "--systohc"])
     print(f"[-] Setting console keymap: {keymap}")
-    #subprocess.run(f'echo "KEYMAP={keymap}" > /mnt/etc/vconsole.conf',chec=True,shell=True)
     cmd(f'echo "KEYMAP={keymap}" > /mnt/etc/vconsole.conf',shell=True)
     run_chroot(["sed", "-i", f"s/^#{locale}/{locale}/", "/etc/locale.gen"])
     run_chroot(["locale-gen"])
-    #subprocess.run(f'echo "LANG={locale}" > /mnt/etc/locale.conf',check=True,shell=True)
     cmd(f'echo "LANG={locale}" > /mnt/etc/locale.conf',shell=True)
     subprocess.run(f'echo "{hostname}" > /mnt/etc/hostname',check=True,shell=True)    
-    #subprocess.run(f'echo "127.0.0.1\tlocalhost\n::1\tlocalhost\n127.0.1.1\t{hostname}.localdomain\t{hostname}" > /mnt/etc/hosts', shell=True)    
     print("[-] Configuring Root password...")
     ps = subprocess.Popen(["echo", f"root:{root_password}"], stdout=subprocess.PIPE)
     subprocess.run(["arch-chroot", "/mnt", "chpasswd"], stdin=ps.stdout, check=True)
